f820760d79b5b19b584c0a3b30a48457824f97e4
Brayan Mendoza <bemendozaa@gmail.com>
2024-02-20 14:59:43 -0500

    base del proyecto - crud products

---
+++ src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductsModule } from './products/products.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(), // con esto ya estan establecidas nuestras variables de entorno
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT, // + para convertir en numero
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true, //cuando hace un cambio en la tabla, automaticamente la sincroniza solo usar en desarrollo
+    }), ProductsModule, CommonModule
+  ],
+})
+export class AppModule {}
